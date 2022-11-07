9c99b9b44dd32fe5ee8d6eab64ab641d9190a128
Daniil <dpancov64@gmail.com>
2022-11-07 20:51:17 +0300

    now the package will work properly, will publish and will update if needed

--- index.js
+++ index.js
@@ -1,3 +1,74 @@
-const eslint = require('./eslintrc.json')
-
-module.exports = eslint
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'prettier',
+    'sort-imports-es6-autofix',
+    'import',
+    'no-relative-import-paths',
+    'sort-destructure-keys',
+    'sort-keys-fix',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:valtio/recommended',
+  ],
+  rules: {
+    'sort-keys-fix/sort-keys-fix': 'error',
+    'valtio/state-snapshot-rule': 'off',
+    'no-relative-import-paths/no-relative-import-paths': [
+      'error',
+      { allowSameFolder: false },
+    ],
+    'import/newline-after-import': ['error'],
+    '@typescript-eslint/no-floating-promises': 'error',
+    'sort-destructure-keys/sort-destructure-keys': [2, { caseSensitive: true }],
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'never', children: 'ignore', propElementValues: 'always' },
+    ],
+    'react/jsx-sort-props': [
+      'error',
+      {
+        callbacksLast: true,
+        shorthandLast: true,
+        multiline: 'first',
+      },
+    ],
+    'require-await': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: false,
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      2,
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+      },
+    ],
+    'import/prefer-default-export': 'error',
+  },
+}
