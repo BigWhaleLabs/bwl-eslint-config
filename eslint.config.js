9a16f78d4727665ebcc01b38b2cff38031571891
T_DaMER <dpancov64@gmail.com>
2023-03-22 21:57:44 +0300

    Use compat to process the stuff

--- eslint.config.js
+++ eslint.config.js
@@ -1,74 +1,95 @@
-module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  plugins: [
-    'react',
-    'react-hooks',
-    '@typescript-eslint',
-    'prettier',
-    'sort-imports-es6-autofix',
-    'import',
-    'no-relative-import-paths',
-    'sort-destructure-keys',
-    'sort-keys-fix',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'plugin:valtio/recommended',
-  ],
-  rules: {
-    'sort-keys-fix/sort-keys-fix': 'error',
-    'valtio/state-snapshot-rule': 'off',
-    'no-relative-import-paths/no-relative-import-paths': [
-      'error',
-      { allowSameFolder: false },
+import { FlatCompat } from '@eslint/eslintrc'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import tsParser from '@typescript-eslint/parser'
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+export default [
+  ...compat.config({
+    env: {
+      es2021: true,
+      node: true,
+    },
+    files: ['.js', '.jsx', '.ts', '.tsx'],
+    parserOptions: {
+      project: './tsconfig.json',
+    },
+    plugins: [
+      'react',
+      'react-hooks',
+      '@typescript-eslint',
+      'prettier',
+      'sort-imports-es6-autofix',
+      'import',
+      'no-relative-import-paths',
+      'sort-destructure-keys',
+      'sort-keys-fix',
     ],
-    'import/newline-after-import': ['error'],
-    '@typescript-eslint/no-floating-promises': 'error',
-    'sort-destructure-keys/sort-destructure-keys': [2, { caseSensitive: true }],
-    'react/jsx-curly-brace-presence': [
-      'error',
-      { props: 'never', children: 'ignore', propElementValues: 'always' },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+      'plugin:valtio/recommended',
     ],
-    'react/jsx-sort-props': [
-      'error',
-      {
-        callbacksLast: true,
-        shorthandLast: true,
-        multiline: 'first',
+    rules: {
+      languageOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+        parser: tsParser,
       },
-    ],
-    'require-await': 'error',
-    'react-hooks/exhaustive-deps': 'error',
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'es5',
-        tabWidth: 2,
-        semi: false,
-        singleQuote: true,
-        endOfLine: 'auto',
-      },
-    ],
-    'sort-imports-es6-autofix/sort-imports-es6': [
-      2,
-      {
-        ignoreCase: false,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-      },
-    ],
-    'import/prefer-default-export': 'error',
-  },
-}
+      'sort-keys-fix/sort-keys-fix': 'error',
+      'valtio/state-snapshot-rule': 'off',
+      'no-relative-import-paths/no-relative-import-paths': [
+        'error',
+        { allowSameFolder: false },
+      ],
+      'import/newline-after-import': ['error'],
+      '@typescript-eslint/no-floating-promises': 'error',
+      'sort-destructure-keys/sort-destructure-keys': [
+        2,
+        { caseSensitive: true },
+      ],
+      'react/jsx-curly-brace-presence': [
+        'error',
+        { props: 'never', children: 'ignore', propElementValues: 'always' },
+      ],
+      'react/jsx-sort-props': [
+        'error',
+        {
+          callbacksLast: true,
+          shorthandLast: true,
+          multiline: 'first',
+        },
+      ],
+      'require-await': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'es5',
+          tabWidth: 2,
+          semi: false,
+          singleQuote: true,
+          endOfLine: 'auto',
+        },
+      ],
+      'sort-imports-es6-autofix/sort-imports-es6': [
+        2,
+        {
+          ignoreCase: false,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        },
+      ],
+      'import/prefer-default-export': 'error',
+    },
+  }),
+]
